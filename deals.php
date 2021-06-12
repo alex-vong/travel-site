b71cda801ed36c4454df131c0c4e1bd1269ff3b8
Alex Vong <avong003@gmail.com>
2021-06-12 02:57:32 -0700

    deals

---
+++ deals.php
@@ -0,0 +1,12 @@
+<section class="deals">
+	<div class="deals-text">
+		<p class="support-text">Deal of the Week</p>
+		<h3 class="big-header">Flights Under $200</h3>
+		<p class="support-text">Fly roundtrip from LAX to Tokyo for Under $200! Travel Anytime from July to November 2021. Fares subject to change.</p>
+		<a href="" class="button"> Book Now</a>
+	</div>
+
+	<picture class="hidden">
+		<img src="images/take-off.png" alt="plane-take-off">
+	</picture>
+</section>
