ddec8f6cec080210d16f41787238f9a0c52689d5
Alex Vong <avong003@gmail.com>
2021-06-12 02:58:09 -0700

    masthead

---
+++ masthead.php
@@ -0,0 +1,12 @@
+<section class="masthead">
+	<nav class="site-menu">
+		<a href="" class="site-logo">Travel&nbsp;<i class="fas fa-plane-departure"></i></a>
+		<a href="#deal-of-day" class="support-text">Deals</a>
+		<a href="spotlight" class="support-text">Spotlight</a>
+		<a href="top-picks" class="support-text">Top Picks</a>
+	</nav>
+
+	<nav class="user-menu">
+		<a href="" class="support-text">Sign-In</a>
+	</nav>
+</section>
