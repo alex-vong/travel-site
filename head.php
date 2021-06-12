16b97ccdb7904c99142c0e68628321b79d0e12b5
Alex Vong <avong003@gmail.com>
2021-06-12 02:57:54 -0700

    head php

---
+++ head.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Travel Site</title>
+		<meta
+			name="description"
+			content="[[meta-description]]"
+		/>
+		<meta
+			name="og:description"
+			content="[[meta-description]]"
+		/>
+	
+		<meta property="og:title" content="[[meta-content]]" />
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+
+		<meta property="og:image" content="[[meta-image]]" />
+		<link rel="shortcut icon" type="image/jpg" href="[[favicon]]" />
+
+		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />	
+		<link href="//db.onlinewebfonts.com/c/f08d8e09fa8cc7b855d5942f89099de4?family=ThirstyRoughBolTwo" rel="stylesheet" type="text/css"/>
+		<link href="//db.onlinewebfonts.com/c/f08d8e09fa8cc7b855d5942f89099de4?family=Thirsty+Rough+Reg+Two" rel="stylesheet" type="text/css"/> 
+
+</head>
