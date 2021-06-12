6d2aaed83eee221d21763fc4e6a56dc08d50489b
Alex Vong <avong003@gmail.com>
2021-06-12 02:58:14 -0700

    search engine

---
+++ search-engine-heading.php
@@ -0,0 +1,27 @@
+<div class="search-field-heading">
+	<h2 class="big-header">Catchy Slogan</h2>
+	<p class="support-text">Alluring text to make you want to spend money</p>
+</div>
+
+<div class="travel-icon-grid">
+	<div class="flight icon">
+		<i class="fas fa-plane"></i>
+		<p class="support-text">Flights</p>
+	</div>
+	<div class="hotel icon">
+		<i class="fas fa-hotel"></i>
+		<p class="support-text">Hotels</p>
+	</div>
+	<div class="car icon">
+		<i class="fas fa-car"></i>
+		<p class="support-text">Cars</p>
+	</div>
+	<div class="cruise icon">
+		<i class="fas fa-anchor"></i>
+		<p class="support-text">Flights</p>
+	</div>
+	<div class="packages icon">
+		<i class="fas fa-luggage-cart"></i>
+		<p class="support-text">Flights</p>
+	</div>
+</div>
