5352f7971312455f15c08184cf8608dc615e5610
Alex Vong <avong003@gmail.com>
2021-06-17 00:49:04 -0700

    spotlight

---
+++ spotlight.php
@@ -0,0 +1,68 @@
+<section class="spotlight-text">
+	<h2 class="site-logo">
+		You're next journey begins here
+	</h2>
+	<p class="support-text">In the Spotlight</p>
+</section>
+
+<section class="destination-grid">
+	<div class="city tokyo">
+		<picture>
+			<img src="images/tokyo-tower.jpeg" alt="">
+		</picture>
+		<div class="city-text">
+			<h3 class="site-logo">Tokyo</h3>
+			<a href="" class="button">Book Now</a>
+		</div>
+	</div>
+
+	<div class="city paris">
+		<picture>
+			<img src="images/paris-night.png" alt="">
+		</picture>
+		<div class="city-text">
+			<h3 class="site-logo">Paris</h3>
+			<a href="" class="button">Book Now</a>
+		</div>
+	</div>
+
+	<div class="city shanghai">
+		<picture>
+			<img src="images/china.jpeg" alt="">
+		</picture>
+		<div class="city-text">
+			<h3 class="site-logo">Shanghai</h3>
+			<a href="" class="button">Book Now</a>
+		</div>
+	</div>
+
+	<div class="city sf">
+		<picture>
+			<img src="images/sf-bay.jpeg" alt="">
+		</picture>
+		<div class="city-text">
+			<h3 class="site-logo">San Francisco</h3>
+			<a href="" class="button">Book Now</a>
+		</div>
+	</div>
+
+	<div class="city new-york">
+		<picture>
+			<img src="images/new-york.jpeg" alt="">
+		</picture>
+		<div class="city-text">
+			<h3 class="site-logo">New York</h3>
+			<a href="" class="button">Book Now</a>
+		</div>
+	</div>
+
+	<div class="city london">
+		<picture>
+			<img src="images/big-ben.jpeg" alt="">
+		</picture>
+		<div class="city-text">
+			<h3 class="site-logo">London</h3>
+			<a href="" class="button">Book Now</a>
+		</div>
+	</div>
+</section>
