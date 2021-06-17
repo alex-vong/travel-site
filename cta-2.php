8761ea41fa54a1d05b82ced2fd279b751eb04cb8
Alex Vong <avong003@gmail.com>
2021-06-17 00:48:49 -0700

    font

---
+++ cta-2.php
@@ -0,0 +1,8 @@
+<section class="deals">
+	<div class="deals-text">
+		<p class="support-text">Deal of the 2021</p>
+		<h3 class="big-header">Plan Your Next Disney Vacation</h3>
+		<p class="support-text">Bring your family to celebrate the re-opening of Disneyland and Disney's California Adventures!</p>
+		<a href="" class="button"> Book Now</a>
+	</div>
+</section>
--- deals.php
+++ deals.php
@@ -5,7 +5,6 @@
 		<p class="support-text">Fly roundtrip from LAX to Tokyo for Under $200! Travel Anytime from July to November 2021. Fares subject to change.</p>
 		<a href="" class="button"> Book Now</a>
 	</div>
-
 	<picture class="hidden">
 		<img src="images/take-off.png" alt="plane-take-off">
 	</picture>
